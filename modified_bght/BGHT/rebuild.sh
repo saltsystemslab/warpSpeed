02c4e2003694848f79a2452670d64cacd7f829a5
huntermBerkeley <hunterm@berkeley.edu>
2025-04-25 19:33:55 -0600

    cleaning up auxillary folders

--- modified_bght/BGHT/rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cuda_arch="86" #specify your GPU SM/gencode
-build_dir="build"
-targets=("all")
-
-cmake -B $build_dir -DCMAKE_CUDA_ARCHITECTURES=${cuda_arch}
-cmake --build $build_dir --target "${targets[@]}" --parallel $(nproc)
